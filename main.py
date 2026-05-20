a1329fba2926c70a89ecf3e527401274168807cb
pinterm5445-beep <pinterm5445@gmail.com>
2026-05-20 14:35:00 +0200

    piros vonal

--- main.py
+++ main.py
@@ -3,4 +3,12 @@ import turtle
 turtle.left(135)
 turtle.forward(100)
 
+
+#pirosvonal
+turtle.goto(0,0)
+turtle.color("red")
+turtle.pensize(5)
+turtle.right(90)
+turtle.forward(100)
+
 turtle.done()
