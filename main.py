1b78d99b8ec031aa097f1a344177107efbe80128
GalaxiDave <galaxidave@gmail.com>
2026-05-22 08:21:14 +0200

    Zöldvonal

--- main.py
+++ main.py
@@ -11,4 +11,11 @@ turtle.pensize(5)
 turtle.right(90)
 turtle.forward(100)
 
+#zöldvonal
+turtle.goto(0,0)
+turtle.color("green")
+turtle.pensize(10)
+turtle.right(-45)
+turtle.forward(100)
+
 turtle.done()
